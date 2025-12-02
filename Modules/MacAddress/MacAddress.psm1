5dbdd22dcfda056e16636abc20db22ee59f61bda
saw-friendship <saw-friendship@yandex.ru>
2025-12-02 23:21:26 +0300

    UseBasicParsing

--- Modules/MacAddress/MacAddress.psm1
+++ Modules/MacAddress/MacAddress.psm1
@@ -83,7 +83,7 @@ function Update-MacAddressDatabase {
     Write-Verbose -Message "Downloading from $Uri" -Verbose
 
     try {
-        Invoke-WebRequest -Uri $Uri -OutFile $OuiFilePath -Headers @{'User-Agent' = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 YaBrowser/24.4.0.0 Safari/537.36'}
+        Invoke-WebRequest -UseBasicParsing -Uri $Uri -OutFile $OuiFilePath -Headers @{'User-Agent' = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 YaBrowser/24.4.0.0 Safari/537.36'}
     } catch {
         throw $_.Exception
     }
